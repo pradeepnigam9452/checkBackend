7f82839550c62a2812fc33e9ecf7d41d53373d01
pradeep <pradeepnigam9452@gmail.com>
2026-03-17 22:21:03 +0530

    Initial backend setup

--- api/index.js
+++ api/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-
 dotenv.config();
 
 const app = express();
@@ -22,4 +21,9 @@ app.get("/", (req, res) => {
   res.json({ message: "Server running 🚀" });
 });
 
+app.get('/data',(req,res)=>{
+  res.send(
+    'chal rhaa  h bhai'
+  )
+})
 export default app;
